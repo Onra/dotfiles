7471d04bf16c55be0012c01c93daf9ecca107686
Arnaud Christ <arnaud.christ@gmail.com>
2022-08-04 23:36:55 +0200

    Put telescope config in separate lua file.

--- nvim/.config/nvim/after/plugin/keymap/init.lua
+++ nvim/.config/nvim/after/plugin/keymap/init.lua
@@ -4,13 +4,6 @@ vim.api.nvim_set_keymap('n', 'yY', '^"+y$', { noremap = true })
 
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true })
 
-vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files hidden=true<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope file_browser<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>fm', '<cmd>Telescope man_pages sections=ALL<CR>', { noremap = true })
-
 vim.api.nvim_set_keymap('n', '<leader>gs', '<cmd>Neogit<CR>', { noremap = false })
 
 -- Better window navigation
