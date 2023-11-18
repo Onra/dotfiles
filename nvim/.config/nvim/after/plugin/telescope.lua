ad493db048d8f4bb86532fd967dc7348013da5e3
Arnaud Christ <arnaud.christ@gmail.com>
2023-11-18 18:31:43 +0100

    Add binding to see Telescope diagnostics.

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -5,4 +5,5 @@ vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
+vim.keymap.set('n', '<leader>fq', builtin.diagnostics, {})
 
