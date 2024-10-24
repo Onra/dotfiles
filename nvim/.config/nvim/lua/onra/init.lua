1213eaad2aa74dc0c6ed4b4bacdafc443baad666
Arnaud Christ <arnaud.christ@gmail.com>
2024-10-24 10:57:12 +0200

    Add tokyonight theme.

--- nvim/.config/nvim/lua/onra/init.lua
+++ nvim/.config/nvim/lua/onra/init.lua
@@ -1,3 +1,5 @@
 require("onra.remap")
 require("onra.set")
 require("onra.lazy")
+
+vim.cmd.colorscheme("tokyonight-night")
--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -6,9 +6,7 @@ return {
         "sindrets/diffview.nvim",        -- optional - Diff integration
 
         -- Only one of these is needed.
-        "nvim-telescope/telescope.nvim", -- optional
         "ibhagwan/fzf-lua",              -- optional
-        "echasnovski/mini.pick",         -- optional
       },
       config = true
     }
