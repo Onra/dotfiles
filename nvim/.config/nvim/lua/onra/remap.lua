49534415421fa11c541f70327c3c10a0beafd402
Arnaud Christ <arnaud.christ@gmail.com>
2024-10-25 11:25:47 +0200

    Add remaps for neogit and netrw.

--- nvim/.config/nvim/lua/onra/remap.lua
+++ nvim/.config/nvim/lua/onra/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true})
+vim.keymap.set('n', '<C-n>', function()
+    if vim.bo.filetype == 'netrw' then
+        vim.cmd 'bwipeout'
+    else
+        vim.cmd 'Explore'
+    end
+end, { silent = true })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true, silent = true })
